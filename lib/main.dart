5877a6e1310e8fa981afe548a791bc2374e78556
Jerszynski <krzysztof.jerszynski@o2.pl>
2022-05-17 15:56:12 +0200

    Changed naming of classes&counters. Separate both players to different containers

--- lib/main.dart
+++ lib/main.dart
@@ -2,69 +2,35 @@ import 'package:flutter/material.dart';
 
 //________________counter 1
 
-class Counter1 extends StatefulWidget {
-  const Counter1({Key? key}) : super(key: key);
+class Player1 extends StatefulWidget {
+  const Player1({Key? key}) : super(key: key);
 
   @override
-  _CounterState createState() => _CounterState();
+  _Player1State createState() => _Player1State();
 }
 
-class _CounterState extends State<Counter1> {
-  int _counter1 = 0;
+class _Player1State extends State<Player1> {
+  int _Player1Counter = 0;
 
   void _increment() {
     setState(() {
-      _counter1++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-          onPressed: _increment,
-          child: const Text('Increment'),
-        ),
-        const SizedBox(width: 16),
-        Text('Count: $_counter1'),
-      ],
-    );
-  }
-}
-
-//________________counter 2
-
-class Counter2 extends StatefulWidget {
-  const Counter2({Key? key}) : super(key: key);
-
-  @override
-  _Counter2State createState() => _Counter2State();
-}
-
-class _Counter2State extends State<Counter2> {
-  int _counter2 = 0;
-
-  void _increment() {
-    setState(() {
-      _counter2++;
+      _Player1Counter++;
     });
   }
 
   void _decrement() {
     setState(() {
-      if (_counter2 != 0) {
-        _counter2--;
+      if (_Player1Counter != 0) {
+        _Player1Counter--;
       } else {
-        _counter2 = 0;
+        _Player1Counter = 0;
       }
     });
   }
 
   void _reset() {
     setState(() {
-      _counter2 = 0;
+      _Player1Counter = 0;
     });
   }
 
@@ -97,7 +63,76 @@ class _Counter2State extends State<Counter2> {
           ],
         ),
         const SizedBox(width: 16),
-        Text('Count: $_counter2'),
+        Text('Count: $_Player1Counter'),
+      ],
+    );
+  }
+}
+
+//________________counter 2
+
+class Player2 extends StatefulWidget {
+  const Player2({Key? key}) : super(key: key);
+
+  @override
+  _Player2State createState() => _Player2State();
+}
+
+class _Player2State extends State<Player2> {
+  int _Player2Counter = 0;
+
+  void _increment() {
+    setState(() {
+      _Player2Counter++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      if (_Player2Counter != 0) {
+        _Player2Counter--;
+      } else {
+        _Player2Counter = 0;
+      }
+    });
+  }
+
+  void _reset() {
+    setState(() {
+      _Player2Counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: ElevatedButton(
+                onPressed: _increment,
+                child: const Text('Increment'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: ElevatedButton(
+                onPressed: _decrement,
+                child: const Text('Decrement'),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _reset,
+              child: const Text('Reset'),
+            ),
+          ],
+        ),
+        const SizedBox(width: 16),
+        Text('Count: $_Player2Counter'),
       ],
     );
   }
@@ -113,20 +148,35 @@ void main() {
         appBar: AppBar(
           title: const Text('Dart Counter'),
         ),
-        body: Container(
-          margin:
-              const EdgeInsets.symmetric(vertical: 200.0, horizontal: 100.0),
-          color: const Color.fromARGB(255, 244, 244, 244),
-          width: 800,
-          height: 400,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Counter1(),
-              Counter2(),
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              // margin: const EdgeInsets.symmetric(
+              //     vertical: 200.0, horizontal: 100.0),
+              color: const Color.fromARGB(255, 244, 244, 244),
+              width: 300,
+              height: 150,
+              child: const Player1(),
+            ),
+            Container(
+              // margin: const EdgeInsets.symmetric(
+              //     vertical: 200.0, horizontal: 100.0),
+              color: const Color.fromARGB(255, 244, 244, 244),
+              width: 300,
+              height: 150,
+              child: const Player2(),
+            ),
+          ],
         ),
+
+        // Column(
+        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //   children: const [
+        //     Counter1(),
+        //     Player2(),
+        //   ],
+        // ),
       ),
     ),
   );
