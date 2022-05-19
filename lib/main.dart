c0d1650d456bc60422c357e18cf3b3e2a9fd717d
Jerszynski <krzysztof.jerszynski@o2.pl>
2022-05-19 10:26:17 +0200

    Change view of player container. Add styling to textfield and text

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,15 @@ class Player1 extends StatefulWidget {
 
 class _Player1State extends State<Player1> {
   int _Player1Counter = 0;
+  int _Games1Counter = 0;
 
   void _increment() {
     setState(() {
       _Player1Counter++;
+      if (_Player1Counter == 6) {
+        _Games1Counter++;
+        _Player1Counter = 0;
+      }
     });
   }
 
@@ -27,6 +32,7 @@ class _Player1State extends State<Player1> {
   void _reset() {
     setState(() {
       _Player1Counter = 0;
+      _Games1Counter = 0;
     });
   }
 
@@ -38,33 +44,54 @@ class _Player1State extends State<Player1> {
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+            Row(
+              children: [
+                Text(
+                  'Games: $_Games1Counter',
+                  style: const TextStyle(
+                    fontSize: 24,
                   ),
                 ),
-                onPressed: _increment,
-                child: const Text('Increment'),
-              ),
+                const SizedBox(
+                  width: 50,
+                ),
+                Text(
+                  'Sets: $_Player1Counter',
+                  style: const TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
                     ),
                   ),
+                  onPressed: _increment,
+                  child: const Text('Increment'),
                 ),
-                onPressed: _decrement,
-                child: const Text('Decrement'),
-              ),
+                const SizedBox(
+                  width: 50,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                  onPressed: _decrement,
+                  child: const Text('Decrement'),
+                ),
+              ],
             ),
             ElevatedButton(
               style: ButtonStyle(
@@ -82,8 +109,6 @@ class _Player1State extends State<Player1> {
             ),
           ],
         ),
-        const SizedBox(width: 16),
-        Text('Count: $_Player1Counter'),
       ],
     );
   }
@@ -100,10 +125,15 @@ class Player2 extends StatefulWidget {
 
 class _Player2State extends State<Player2> {
   int _Player2Counter = 0;
+  int _Games2Counter = 0;
 
   void _increment() {
     setState(() {
       _Player2Counter++;
+      if (_Player2Counter == 6) {
+        _Games2Counter++;
+        _Player2Counter = 0;
+      }
     });
   }
 
@@ -116,6 +146,7 @@ class _Player2State extends State<Player2> {
   void _reset() {
     setState(() {
       _Player2Counter = 0;
+      _Games2Counter = 0;
     });
   }
 
@@ -127,33 +158,54 @@ class _Player2State extends State<Player2> {
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+            Row(
+              children: [
+                Text(
+                  'Games: $_Games2Counter',
+                  style: const TextStyle(
+                    fontSize: 24,
                   ),
                 ),
-                onPressed: _increment,
-                child: const Text('Increment'),
-              ),
+                const SizedBox(
+                  width: 50,
+                ),
+                Text(
+                  'Sets: $_Player2Counter',
+                  style: const TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
                     ),
                   ),
+                  onPressed: _increment,
+                  child: const Text('Increment'),
                 ),
-                onPressed: _decrement,
-                child: const Text('Decrement'),
-              ),
+                const SizedBox(
+                  width: 50,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                  onPressed: _decrement,
+                  child: const Text('Decrement'),
+                ),
+              ],
             ),
             ElevatedButton(
               style: ButtonStyle(
@@ -162,18 +214,15 @@ class _Player2State extends State<Player2> {
                     borderRadius: BorderRadius.circular(8.0),
                   ),
                 ),
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    Color.fromARGB(255, 243, 111, 102)),
-                shadowColor: MaterialStateProperty.all<Color>(
-                    Color.fromARGB(255, 0, 0, 0)),
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+                shadowColor: MaterialStateProperty.all(Colors.black),
+                elevation: MaterialStateProperty.all(10),
               ),
               onPressed: _reset,
               child: const Text('Reset'),
             ),
           ],
         ),
-        const SizedBox(width: 16),
-        Text('Count: $_Player2Counter'),
       ],
     );
   }
@@ -186,50 +235,87 @@ void main() {
     MaterialApp(
       title: 'Simple Dart Counter for 2 ppl.',
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Dart Counter'),
+        appBar: AppBar(
+          title: const Text('Dart Counter'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 300,
+                child: TextField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(2),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(),
+                      ),
+                      prefixIcon: const Icon(Icons.person),
+                      hintText: 'Enter player name'),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 244, 244, 244),
+                  borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 15,
+                      offset: const Offset(4, 4),
+                    ),
+                  ],
+                ),
+                width: 300,
+                height: 180,
+                child: const Player1(),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(2),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(),
+                      ),
+                      prefixIcon: Icon(Icons.person),
+                      hintText: 'Enter player name'),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 244, 244, 244),
+                  borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 15,
+                      offset: const Offset(4, 4),
+                    ),
+                  ],
+                ),
+                width: 300,
+                height: 180,
+                child: const Player2(),
+              ),
+            ],
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 244, 244, 244),
-                    borderRadius: const BorderRadius.all(Radius.circular(12)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 15,
-                        offset: const Offset(4, 4),
-                      ),
-                    ],
-                  ),
-                  width: 300,
-                  height: 150,
-                  child: const Player1(),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 244, 244, 244),
-                    borderRadius: const BorderRadius.all(Radius.circular(12)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 15,
-                        offset: const Offset(4, 4),
-                      ),
-                    ],
-                  ),
-                  width: 300,
-                  height: 150,
-                  child: const Player2(),
-                ),
-              ],
-            ),
-          )),
+        ),
+      ),
     ),
   );
 }
