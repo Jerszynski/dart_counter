daafad20c7a8f512745ae357bcbd4b1a9afc913f
Jerszynski <krzysztof.jerszynski@o2.pl>
2022-06-10 00:24:40 +0200

    Change DartMain to HomePage. Create one class for both text players fields

--- lib/main.dart
+++ lib/main.dart
@@ -3,44 +3,20 @@ import 'package:dart_counter/players/player2.dart';
 import 'package:dart_counter/scores/scores.dart';
 import 'package:flutter/material.dart';
 
-//-------------------- APP
-
 void main() {
   runApp(
     const MaterialApp(
       title: 'Simple Dart Counter for 2 ppl.',
-      home: DartMain(),
+      home: HomePage(),
     ),
   );
 }
 
-class DartMain extends StatelessWidget {
-  const DartMain({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final firstPlayerTextField = TextField(
-      // autofocus: true,
-      decoration: InputDecoration(
-        contentPadding: const EdgeInsets.all(2),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(color: Colors.black, width: 2.0),
-        ),
-        fillColor: Colors.white.withOpacity(0.7),
-        filled: true,
-        prefixIcon: const Icon(Icons.person),
-        hintText: 'Enter player name',
-        hintStyle: const TextStyle(
-          color: Colors.black26,
-        ),
-        labelText: 'Player 1',
-        labelStyle: const TextStyle(
-          color: Colors.black45,
-        ),
-      ),
-    );
-
     final firstPlayerContainer = Container(
       decoration: BoxDecoration(
         color: const Color.fromARGB(255, 244, 244, 244).withOpacity(0.7),
@@ -52,27 +28,6 @@ class DartMain extends StatelessWidget {
       ),
       height: 180,
       child: const Player1(),
-    );
-
-    final secondPlayerTextField = TextField(
-      decoration: InputDecoration(
-        contentPadding: const EdgeInsets.all(2),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(color: Colors.black, width: 2.0),
-        ),
-        fillColor: Colors.white.withOpacity(0.7),
-        filled: true,
-        prefixIcon: const Icon(Icons.person),
-        hintText: 'Enter player name',
-        hintStyle: const TextStyle(
-          color: Colors.black26,
-        ),
-        labelText: 'Player 2',
-        labelStyle: const TextStyle(
-          color: Colors.black45,
-        ),
-      ),
     );
 
     final secondPLayerContainer = Container(
@@ -131,11 +86,11 @@ class DartMain extends StatelessWidget {
               padding: const EdgeInsets.all(40),
               physics: const NeverScrollableScrollPhysics(),
               children: [
-                firstPlayerTextField,
+                const TextFields('Player 1'),
                 const SizedBox(height: 10),
                 firstPlayerContainer,
                 const SizedBox(height: 40),
-                secondPlayerTextField,
+                const TextFields('Player 2'),
                 const SizedBox(height: 10),
                 secondPLayerContainer,
                 const SizedBox(height: 20),
@@ -144,6 +99,40 @@ class DartMain extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class TextFields extends StatelessWidget {
+  const TextFields(
+    this.fieldTitle, {
+    Key? key,
+  }) : super(key: key);
+
+  final String fieldTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      // autofocus: true,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(2),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Colors.black, width: 2.0),
+        ),
+        fillColor: Colors.white.withOpacity(0.7),
+        filled: true,
+        prefixIcon: const Icon(Icons.person),
+        hintText: 'Enter player name',
+        hintStyle: const TextStyle(
+          color: Colors.black26,
+        ),
+        labelText: fieldTitle,
+        labelStyle: const TextStyle(
+          color: Colors.black45,
+        ),
       ),
     );
   }
