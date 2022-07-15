cec098ae01e2dbdf605c80f2a1c29a178d2fbc07
Jerszynski <krzysztof.jerszynski@o2.pl>
2022-07-15 23:34:11 +0200

    Add layout with first position with flutter switch

--- lib/settings_content/settings_content.dart
+++ lib/settings_content/settings_content.dart
@@ -1,12 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool status = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: status ? Colors.grey.shade800 : Colors.grey.shade200,
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: const BorderRadius.all(Radius.circular(25)),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 12.0,
+                      spreadRadius: 1.0,
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(0.0, -2.0),
+                      blurRadius: 12.0,
+                      spreadRadius: 1.0,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'SETTINGS',
+                            style: TextStyle(
+                              fontSize: 24,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text('BottomNavigationBar \nlight/dark mode',
+                              textAlign: TextAlign.left),
+                          FlutterSwitch(
+                            width: 50.0,
+                            height: 25.0,
+                            padding: 6.0,
+                            showOnOff: true,
+                            valueFontSize: 12.0,
+                            toggleSize: 12.0,
+                            activeColor: Colors.green,
+                            value: status,
+                            onToggle: (value) {
+                              setState(() {
+                                status = value;
+                              });
+                            },
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
