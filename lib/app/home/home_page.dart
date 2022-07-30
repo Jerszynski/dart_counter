b4b04702654ebb64a3a5813e665bc37fa58a65ff
Jerszynski <krzysztof.jerszynski@o2.pl>
2022-07-30 16:46:40 +0200

    Fix BottomNavigationBar display with 4 items

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -39,6 +39,7 @@ class _HomePageState extends State<HomePage> {
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         currentIndex: currentIndex,
         onTap: (newIndex) {
           setState(() {
