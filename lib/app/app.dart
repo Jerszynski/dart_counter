5676347df8ccf95ddf7089a168b7a119b166ad5a
Jerszynski <krzysztof.jerszynski@o2.pl>
2022-10-02 01:24:26 +0200

    Move RootPage to app.dart

---
+++ lib/app/app.dart
@@ -0,0 +1,26 @@
+import 'package:dart_counter/app/home/home_page.dart';
+import 'package:dart_counter/app/login/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        final user = snapshot.data;
+        if (user == null) {
+          return LoginPage();
+        }
+        return HomePage(
+          user: user,
+        );
+      },
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child dart_counter in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:dart_counter/main.dart';
+import 'package:dart_counter/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
